29881a4d3231baebe8c866a78ccd73740d5e88cd
JLontiveros <louis.ontiveros20@gmail.com>
2024-11-17 18:17:11 +0800

    finalfinal commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -10,5 +9,17 @@ export default defineConfig({
     sourcemap: false,
     minify: true,
     chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'scheduler']
   }
 })
